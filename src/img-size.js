4cb445b40c72b5829a8c8224f9c8e4629b8f5e19
Bartosz Lorek <bery.lorek@gmail.com>
2017-04-02 23:10:12 +0200

    bindedImages, basic imageResize aka cover size; throttled and debounced resize; clean image-load

--- src/img-size.js
+++ src/img-size.js
@@ -1,20 +1,88 @@
 import loadImage from './load-image.js';
+import addResize from './resize.js';
 
-function getDirection(element) {
-    loadImage(element, (img) => {
-        console.log(img, img.width, img.height)
-    })
-    return 'v';
+const HORIZONTAL = 'img-size-h';
+const VERTICAL = 'img-size-v';
+const regex = new RegExp('\\s*('+ HORIZONTAL +'|'+ VERTICAL +')');
+
+function imageResize(image) {
+    let imageRatio,
+        direction;
+
+    return function() {
+        loadImage(image, (size) => {
+            if (! imageRatio) {
+                  imageRatio = size.height > 0
+                ? size.width / size.height
+                : 0;
+            }
+            let parent = image.parentElement,
+                parentWidth = parent.clientWidth,
+                parentHeight = parent.clientHeight;
+
+            let altImageWidth = parentHeight * imageRatio,
+                altImageHeight = parentWidth / imageRatio,
+                hFactor = parentWidth - altImageWidth,
+                vFactor = parentHeight - altImageHeight;
+
+            let newDirection = hFactor > vFactor ? HORIZONTAL : VERTICAL;
+            if (newDirection === direction) return;
+            
+            direction = newDirection;
+            image.className = image.className.replace(regex, '');
+            image.className += ' ' + direction;
+        })
+    }
 }
 
-function addSizeClass(element) {
-    let dir = getDirection(element);
+const bindedImages = (() => {
+    let images = [];
+    let onResize = addResize(() => {
+        let length = images.length;
+        for (let i=0; i<length; i++) {
+            images[i].action();
+        }
+    }, 100);
+
+    function addImage(element) {
+        images.push({
+            image: element,
+            action: imageResize(element)
+        })
+    }
+    return {
+        add: function(element) {
+            if (! element) return;
+            if (element.nodeType) {
+                addImage(element);
+            }
+            else if (element.length) {
+                let length = element.length;
+                for (let i=0; i<length; i++) {
+                    addImage(element[i]);
+                }
+            }
+            return this;
+        },
+        remove: function(element) {
+            if (element && element.nodeType) {
+                images = images.filter(item =>
+                    item.image !== element)
+            }
+            return this;
+        },
+        call: function() {
+            onResize();
+            return this;
+        },
+        get items() {
+            return images;
+        }
+    }
+})();
 
 
-    console.log(dir)
-}
+let images = document.getElementsByClassName('img-size'),
+    wide = document.getElementById('img-wide');
 
-let images = document.getElementsByClassName('img-size');
-for (let i=0, length=images.length; i<length; i++) {
-    addSizeClass(images[i]);
-}
+bindedImages.add(images).call();
--- src/load-image.js
+++ src/load-image.js
@@ -1,19 +1,31 @@
+let loading = [];
 
 function loadImage(element, callback) {
-    let src = element && element.nodeType
-           && element.getAttribute('src'),
-        image;
+    let image;
 
-    if (typeof src !== 'string') {
-        return false;
+    if (typeof callback !== 'function') return;
+    if (! (element && element.src !== '')) return;
+    if (loading.indexOf(element.src) !== -1) {
+        return;
     }
+    if (element.complete) {
+        callback({
+            width: element.naturalWidth,
+            height: element.naturalHeight
+        });
+        return;
+    }
+    loading.push(element.src);
     image = new Image();
     image.onload = function() {
-        if (typeof callback === 'function') {
-            callback(this);
-        }  
+        let index = loading.indexOf(this.src);
+        loading.splice(index, 1);
+        callback({
+            width: this.width,
+            height: this.height
+        });
     }
-    image.src = src;
+    image.src = element.src;
 }
 
  export default loadImage;
