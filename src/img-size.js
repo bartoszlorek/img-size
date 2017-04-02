33afe27a98315e0f09eeb82a3aa4b4dba8de2959
Bartosz Lorek <bery.lorek@gmail.com>
2017-04-03 00:22:24 +0200

    cover and contain methods

--- src/img-size.js
+++ src/img-size.js
@@ -3,7 +3,37 @@ import addResize from './resize.js';
 
 const HORIZONTAL = 'img-size-h';
 const VERTICAL = 'img-size-v';
+const COVER = 'img-size-cover';
+const CONTAIN = 'img-size-contain';
 const regex = new RegExp('\\s*('+ HORIZONTAL +'|'+ VERTICAL +')');
+
+
+function edgeOffset(imageRatio, parent) {
+    let parentWidth = parent.clientWidth,
+        parentHeight = parent.clientHeight,
+        boundWidth = parentHeight * imageRatio,
+        boundHeight = parentWidth / imageRatio;
+    return {
+        width: parentWidth - boundWidth,
+        height: parentHeight - boundHeight
+    }
+}
+
+function sizeCover(imageRatio, parent) {
+    let offset = edgeOffset(imageRatio, parent);
+    return offset.width > offset.height ? HORIZONTAL : VERTICAL;
+}
+
+function sizeContain(imageRatio, parent) {
+    let offset = edgeOffset(imageRatio, parent);
+    return offset.width < offset.height ? HORIZONTAL : VERTICAL;
+}
+
+function sizeType(image) {
+    if (image.className.indexOf(CONTAIN) !== -1)
+        return CONTAIN;
+    return COVER;
+}
 
 function imageResize(image) {
     let imageRatio,
@@ -16,18 +46,11 @@ function imageResize(image) {
                 ? size.width / size.height
                 : 0;
             }
-            let parent = image.parentElement,
-                parentWidth = parent.clientWidth,
-                parentHeight = parent.clientHeight;
-
-            let altImageWidth = parentHeight * imageRatio,
-                altImageHeight = parentWidth / imageRatio,
-                hFactor = parentWidth - altImageWidth,
-                vFactor = parentHeight - altImageHeight;
-
-            let newDirection = hFactor > vFactor ? HORIZONTAL : VERTICAL;
-            if (newDirection === direction) return;
-            
+            let method = sizeType(image) === COVER ? sizeCover : sizeContain,
+                newDirection = method(imageRatio, image.parentElement);
+            if (newDirection === direction) {
+                return;
+            }
             direction = newDirection;
             image.className = image.className.replace(regex, '');
             image.className += ' ' + direction;
@@ -42,7 +65,7 @@ const bindedImages = (() => {
         for (let i=0; i<length; i++) {
             images[i].action();
         }
-    }, 100);
+    }, 100, 100);
 
     function addImage(element) {
         images.push({
