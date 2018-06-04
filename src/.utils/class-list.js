5da4402f421cc753a9908c8e68cafbaf7ed25dfe
Bartosz Lorek <bery.lorek@gmail.com>
2018-06-05 01:28:54 +0200

    handle multiple classNames

--- src/.utils/class-list.js
+++ src/.utils/class-list.js
@@ -1,3 +1,20 @@
+function baseAddClass(element, className) {
+    if (element.classList) {
+        element.classList.add(className)
+    } else if (!hasClass(element, className)) {
+        element.className += ' ' + className
+    }
+}
+
+function baseRemoveClass(element, className) {
+    if (element.classList) {
+        element.classList.remove(className)
+    } else if (hasClass(element, className)) {
+        let reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
+        element.className = element.className.replace(reg, ' ')
+    }
+}
+
 export function hasClass(element, className) {
     if (element.classList) {
         return element.classList.contains(className)
@@ -7,18 +24,25 @@ export function hasClass(element, className) {
 }
 
 export function addClass(element, className) {
-    if (element.classList) {
-        element.classList.add(className)
-    } else if (!hasClass(element, className)) {
-        element.className += ' ' + className
+    if (typeof className === 'string') {
+        baseAddClass(element, className)
+    } else {
+        let index = -1
+        const length = className.length || 0
+        while (++index < length) {
+            baseAddClass(element, className[index])
+        }
     }
 }
 
 export function removeClass(element, className) {
-    if (element.classList) {
-        element.classList.remove(className)
-    } else if (hasClass(element, className)) {
-        let reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
-        element.className = element.className.replace(reg, ' ')
+    if (typeof className === 'string') {
+        baseRemoveClass(element, className)
+    } else {
+        let index = -1
+        const length = className.length || 0
+        while (++index < length) {
+            baseRemoveClass(element, className[index])
+        }
     }
 }
