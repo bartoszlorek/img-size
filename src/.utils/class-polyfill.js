9523e16f9a4cf76b93fa64c51a7bba93c82c13ef
Bartosz Lorek <bery.lorek@gmail.com>
2018-06-11 00:56:18 +0200

    clean code

---
+++ src/.utils/class-polyfill.js
@@ -0,0 +1,42 @@
+const classToArray = value => {
+    return typeof value === 'string' ? value.split(/\s+/) : value || []
+}
+
+export function hasClass(element, className) {
+    if (element.classList) {
+        return element.classList.contains(className)
+    } else {
+        return !!(element.className.indexOf(className) > -1)
+    }
+}
+
+export function addClass(element, className) {
+    let classes = classToArray(className)
+    if (element.classList) {
+        element.classList.add.apply(null, classes)
+    } else {
+        let result = element.className
+        for (let i = 0; i < classes.length; i++) {
+            if (result.indexOf(classes[i]) < 0) {
+                result += ' ' + classes[i]
+            }
+        }
+        element.className = result
+    }
+}
+
+export function removeClass(element, className) {
+    let classes = classToArray(className)
+    if (element.classList) {
+        element.classList.remove.apply(null, classes)
+    } else {
+        let current = classToArray(element.className),
+            result = ''
+        for (let i = 0; i < current.length; i++) {
+            if (classes.indexOf(current[i]) < 0) {
+                result += ' ' + current[i]
+            }
+        }
+        element.className = result.substring(1)
+    }
+}
